d79fbf6dad58ba25c0a0ce1fc3e9425f69c7eb63
sj332 <112758738+sj332@users.noreply.github.com>
2022-10-04 17:06:51 +0900

    Add files via upload

--- mainreal.c
+++ mainreal.c
@@ -4,17 +4,19 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int x;
-	  
-	printf("정수 하나를 입력하시오:");
-	scanf("%d",&x);
+	char c; //문자를 받기 위함 
+	int cnt=0;
 	
-	if (x>0) //양수인 경우 
-		printf("양수입니다.");
-	else if (x==0) //0인 경우 
-		printf("0입니다.");
-	else //음수인 경우 
-		printf("음수입니다.");
-		 
+	printf("input a string:");
+	/*scanf("%s",char);*/
+	while((c=getchar())!='\n') //문자 입력 ## 입력문자가 개행문자가 나올 때까지 반복 
+	{if (c>='0' && c<='9') //숫자면 0~9임을 이용 
+		cnt++;//반복 ## 입력된 글자가 숫자인가?
+			// 그렇다면 하나를 센다. 
+	}
+	
+	printf("the number of digits is %i.\n",cnt);
+
+
 	return 0;
 }
