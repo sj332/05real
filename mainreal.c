cf7a0f5d1711f48554106cedf054e1ce9d3b0d93
sj332 <112758738+sj332@users.noreply.github.com>
2022-10-04 17:37:28 +0900

    Add files via upload

--- mainreal.c
+++ mainreal.c
@@ -4,19 +4,46 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	char c; //문자를 받기 위함 
-	int cnt=0;
+	int x,y;
+	char op;//연산자 operator
+	int result;
 	
-	printf("input a string:");
-	/*scanf("%s",char);*/
-	while((c=getchar())!='\n') //문자 입력 ## 입력문자가 개행문자가 나올 때까지 반복 
-	{if (c>='0' && c<='9') //숫자면 0~9임을 이용 
-		cnt++;//반복 ## 입력된 글자가 숫자인가?
-			// 그렇다면 하나를 센다. 
+	printf("enter the calculation :");
+	scanf("%d %c %d",&x,&op,&y);
+	
+	//연산자에 따라 결과값을 계산
+	
+	/*
+ 	if(op=='+')
+ 		result=x+y;
+ 	else if(op=='-')
+ 		result=x-y;
+ 	else if(op=='*')
+ 		result=x*y;
+ 	else 
+ 		result=x/y;
+	*/
+	
+	switch(op)
+	{
+		case '+':
+			result=x+y;
+			break;
+			
+		case '-':
+			result=x-y;
+			break;
+		case '*':
+			result=x*y;
+			break;
+			 
+		default :
+			result=x/y;
+			break;
 	}
 	
-	printf("the number of digits is %i.\n",cnt);
-
-
+	printf("=%i\n", result);
+	 
+	
 	return 0;
 }
